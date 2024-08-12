69faa9abaaacb0aaa14de9d33c5b1ca2bbe2a70a
Lavinia Loredana <138888622+lavinia-scode@users.noreply.github.com>
2024-08-12 20:08:46 +0200

    feat: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import "./App.css";
+import { Box, Button, Heading } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <h1>Developer Portal</h1>
-      <p>
-        Everything you need to creare and manage your organization, all in one
-        place
-      </p>
+      <Box>
+        <Heading>Developer Portal </Heading>
+        <Box padding="5">
+          <p>
+            Everything you need to create and manage your organization, all in
+            one place
+          </p>
+        </Box>
 
-      <button
-        onClick={() => console.log("Lavinia is trying to sign in")}
-      >
-        Sign in
-      </button>
+        <Button
+          colorScheme="pink"
+          size="lg"
+          onClick={() => console.log("Lavinia is trying to sign in")}
+        >
+          Sign In
+        </Button>
+      </Box>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+} from "@chakra-ui/react";
 
+import styles from "./theme.tsx";
 
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const theme = extendTheme({ colors });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={styles} cssVarsRoot="body">
       <App />
     </ChakraProvider>
   </StrictMode>
